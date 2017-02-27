7ba2a1718408b7e73576ad484c0df3f8f5c752bf
xinxiu.zhang <xinxiu.zhang@corp.elong.com>
2017-02-27 20:34:10 +0800

    添加内容

--- picfix/frmMain.Designer.cs
+++ picfix/frmMain.Designer.cs
@@ -28,11 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.tsmFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tslmIBatchFix = new System.Windows.Forms.ToolStripMenuItem();
+            this.tlmlIReduce = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(1048, 25);
+            this.mnuStrip.TabIndex = 0;
+            this.mnuStrip.Text = "菜单导航";
+            // 
+            // tsmFile
+            // 
+            this.tsmFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslmIBatchFix});
+            this.tsmFile.Name = "tsmFile";
+            this.tsmFile.Size = new System.Drawing.Size(44, 21);
+            this.tsmFile.Text = "文件";
+            // 
+            // tslmIBatchFix
+            // 
+            this.tslmIBatchFix.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tlmlIReduce});
+            this.tslmIBatchFix.Name = "tslmIBatchFix";
+            this.tslmIBatchFix.Size = new System.Drawing.Size(152, 22);
+            this.tslmIBatchFix.Text = "批量处理";
+            // 
+            // tlmlIReduce
+            // 
+            this.tlmlIReduce.Name = "tlmlIReduce";
+            this.tlmlIReduce.Size = new System.Drawing.Size(152, 22);
+            this.tlmlIReduce.Text = "压缩";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 714);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmMain";
             this.Text = "frmMain";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem tsmFile;
+        private System.Windows.Forms.ToolStripMenuItem tslmIBatchFix;
+        private System.Windows.Forms.ToolStripMenuItem tlmlIReduce;
     }
 }
